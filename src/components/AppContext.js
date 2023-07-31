b599e53ff3d8bb6470007b8f8d5e529e81bf033f
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 22:47:45 +0300

    added case in switch

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -1,6 +1,10 @@
 import { createContext, useReducer } from "react"
 const AppReducer = (state, action) => {
   switch(action.type){
+    case 'ADD_EXPENSE':
+      return{
+        ...state, expenses: [...state.expenses, action.payload]
+      }
     default:
       return state;
   }
