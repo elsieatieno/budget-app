828290f3a69d7aa53344dadd0fd79beed3f2667a
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 22:56:24 +0300

    used context

--- src/components/Spent.js
+++ src/components/Spent.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AppContext } from './AppContext'
 
 const Spent = () => {
+  const{expenses} = useContext(AppContext)
+
+  const total = expenses.reduce((total, item)=>{
+    return total = total + item.cost
+  }, 0)
+  
   return (
     <div className='alert alert-primary'>
-      <span>Spent so far: Ksh. 1000</span>
+      <span>Spent so far: Ksh. {total}</span>
     </div>
   )
 }
