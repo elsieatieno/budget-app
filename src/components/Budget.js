948410cc3d543c0cba7ee5f122a1b53094d981e1
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 22:31:47 +0300

    Used context hook

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AppContext } from './AppContext'
 
 const Budget = () => {
+  const {budget} = useContext(AppContext)
   return (
     <div className='alert alert-secondary'>
-      <span>Budget: Ksh. 2000</span>
+      <span>Budget: Ksh. {budget}</span>
     </div>
   )
 }
