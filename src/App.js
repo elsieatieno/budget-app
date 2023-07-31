e688152e22bdeffee7a005b34f53ff95cd6becbb
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 22:25:25 +0300

    wrapped with context

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Budget from './components/Budget'
 import Remaining from "./components/Remaining";
 import Spent from "./components/Spent";
 import ExpenseList from "./components/ExpenseList";
+import AddForm from "./components/AddForm";
+import { AppProvider } from "./components/AppContext";
 
 function App() {
   return (
+  <AppProvider>
    <div className="container">
     <h1 className="mt-3">My Budget Planner</h1>
-    
+
     <div className="row mt-3">
       <div className="col-sm">
         <Budget />
@@ -28,8 +31,14 @@ function App() {
         </div>
        </div>
     </div>
+    <h3 className="mt-3">Add Expenses</h3>
+    <div className="row mt-3">
+      <div className="col-sm">
+        <AddForm />
+      </div>
+    </div>
    </div>
-
+   </AppProvider>
   );
 }
 
