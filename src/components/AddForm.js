57d5712f95a388412b249a938c129dbdb788d5d9
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 22:47:09 +0300

    used context hook

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState,useContext } from 'react'
+import { AppContext } from './AppContext'
+import {v4 as uuidv4} from 'uuid';
 
 const AddForm = () => {
+  const{dispatch} =useContext(AppContext)
+  const [name, setName]= useState('')
+  const [cost, setCost] = useState('')
+  const onsubmit = (event)=>{
+    event.preventDefault()
+
+    const expense = {
+      id: uuidv4(),
+      name:name,
+      cost: parseInt(cost)
+    }
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    })
+  }
+  
   return (
     <div>
-      <form>
+      <form onSubmit={onsubmit}>
         <div className='row'>
           <div className='col-sm'>
             <label for='name'>Name</label>
-            <input required='required' type='text' className='form-control' id='name'/>
+            <input required='required' type='text' className='form-control' id='name' value={name}
+            onChange={(event)=>{setName(event.target.value)}}/>
           </div>
           <div className='col-sm'>
             <label for='name'>Cost</label>
-            <input required='required' type='text' className='form-control' id='cost'/>
+            <input required='required' type='text' className='form-control' id='cost' value={cost}
+            onChange={(event)=>{setCost(event.target.value)}}/>
           </div>
           <div className='col-sm'>
-            <button type='submit' className='btn btn-primary'> Save </button>
+            <button type='submit' className='btn btn-primary' > Save </button>
           </div>
         </div>
       </form>
