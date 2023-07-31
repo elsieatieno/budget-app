2bf47af7f40d0c5d7b35309b9a713b9cc8258e7a
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 22:30:22 +0300

    Added expenses

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -11,6 +11,8 @@ const initialState = {
   expenses:[
     { id: 1, name: 'shopping', cost: 200 },
     { id: 2, name: 'transport', cost: 250 },
+    { id: 3, name: 'fuel', cost: 250 },
+    { id: 4, name: 'health', cost: 300 },
   ],
 }
 
