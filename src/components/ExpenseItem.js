ebbce701f93d304137a6b3dfffe093e17805cd41
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 23:01:30 +0300

    created delete function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {TiDelete} from 'react-icons/ti'
+import { AppProvider } from './AppContext'
 
 const ExpenseItem = ({id, name, cost}) => {
+  const {dispatch} = useContext(AppProvider)
+
+  const handleDelete = () =>{
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: id,
+    })
+  }
   return (
     <div>
       <li key={id} className='list-group-item d-flex justify-content-between align-items-center'>
         {name}
         <span className='badge badge-primary badge-pill mr-3'>{cost}</span>
-        <TiDelete size='1.5em'></TiDelete>
+        <TiDelete size='1.5em' onClick={handleDelete }></TiDelete>
       </li>
     </div>
   )
