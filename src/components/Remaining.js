2b29e99f3ec6adbef61ece2501115c00b66202af
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 21:21:46 +0300

    Given class name

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Remaining = () => {
   return (
-    <div className=''>
+    <div className='alert alert-success'>
       <span>Remaining: Ksh. 1000</span>
     </div>
   )
