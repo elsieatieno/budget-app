c1f700fcf4be1004af1b1ca68718c31cc67a0d7e
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-31 23:09:13 +0300

    Completed project

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,46 +1,67 @@
-import React, { useState,useContext } from 'react'
-import { AppContext } from './AppContext'
-import {v4 as uuidv4} from 'uuid';
+import React, { useState, useContext } from 'react';
+import { AppContext } from './AppContext';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddForm = () => {
-  const{dispatch} =useContext(AppContext)
-  const [name, setName]= useState('')
-  const [cost, setCost] = useState('')
-  const onsubmit = (event)=>{
-    event.preventDefault()
+  const { dispatch } = useContext(AppContext);
+  const [name, setName] = useState('');
+  const [cost, setCost] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
 
     const expense = {
       id: uuidv4(),
-      name:name,
+      name: name,
       cost: parseInt(cost)
-    }
+    };
     dispatch({
       type: 'ADD_EXPENSE',
       payload: expense,
-    })
-  }
-  
+    });
+    setName('');
+    setCost('');
+  };
+
   return (
     <div>
-      <form onSubmit={onsubmit}>
+      <form onSubmit={onSubmit}>
         <div className='row'>
           <div className='col-sm'>
-            <label for='name'>Name</label>
-            <input required='required' type='text' className='form-control' id='name' value={name}
-            onChange={(event)=>{setName(event.target.value)}}/>
+            <label htmlFor='name'>Name</label>
+            <input
+              required
+              type='text'
+              className='form-control'
+              id='name'
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
           </div>
           <div className='col-sm'>
-            <label for='name'>Cost</label>
-            <input required='required' type='text' className='form-control' id='cost' value={cost}
-            onChange={(event)=>{setCost(event.target.value)}}/>
+            <label htmlFor='cost'>Cost</label>
+            <input
+              required
+              type='text'
+              className='form-control'
+              id='cost'
+              value={cost}
+              onChange={(event) => {
+                setCost(event.target.value);
+              }}
+            />
           </div>
           <div className='col-sm'>
-            <button type='submit' className='btn btn-primary' > Save </button>
+            <button type='submit' className='btn btn-primary'>
+              Save
+            </button>
           </div>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddForm
+export default AddForm;
